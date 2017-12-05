7bdff0a4ac6ca74284bde31b851a4845273eeec8
BastienBellariva <bastien.bellariva@ynov.com>
2017-12-05 16:47:07 +0100

    IG Header OK

--- platforms/ios/HelloCordova.xcarchive/Products/Applications/HelloCordova.app/www/js/index.js
+++ platforms/ios/HelloCordova.xcarchive/Products/Applications/HelloCordova.app/www/js/index.js
@@ -1,21 +1,11 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
+
+
+$("#test").click(function(){
+    app.test();
+});
+
+
+
 var app = {
     // Application Constructor
     initialize: function() {
@@ -28,6 +18,12 @@ var app = {
     // 'pause', 'resume', etc.
     onDeviceReady: function() {
         this.receivedEvent('deviceready');
+        var date = this.date();
+        var heure = this.heure();
+        date = date + " - " + heure;
+        console.log(cordova.file);
+        //alert(date);
+        //setInterval(this.alarm);
     },
 
     // Update DOM on a Received Event
@@ -40,7 +36,93 @@ var app = {
         receivedElement.setAttribute('style', 'display:block;');
 
         console.log('Received Event: ' + id);
-    }
+    },
+
+    date: function(){
+        // les noms de jours / mois
+        var jours = new Array("dimanche", "lundi", "mardi", "mercredi", "jeudi", "vendredi", "samedi");
+        var mois = new Array("janvier", "fevrier", "mars", "avril", "mai", "juin", "juillet", "aout", "septembre", "octobre", "novembre", "decembre");
+        // on recupere la date
+        var date = new Date();
+        // on construit le message
+        var message = jours[date.getDay()] + " ";   // nom du jour
+        message += date.getDate() + " ";   // numero du jour
+        message += mois[date.getMonth()] + " ";   // mois
+        message += date.getFullYear();
+        return message;
+    },
+
+    heure: function(){
+         var date = new Date();
+         var heure = date.getHours();
+         var minutes = date.getMinutes();
+         if(minutes < 10)
+              minutes = "0" + minutes;
+         if (heure < 10)
+              heure = "0" + heure;  
+         return heure + "h" + minutes;
+    },
+
+    alarm: function(){
+
+    },
+
+    test: function(){
+    	var jsonArray = {"name": "Chris", "age": "38"};
+		var myJsonString = JSON.stringify(jsonArray);
+		window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, function (fs) {
+
+	    	console.log('file system open: ' + fs.name);
+	    	fs.root.getFile("test.json", { create: true, exclusive: false }, function (fileEntry) {
+
+	        	console.log("fileEntry is file?" + fileEntry.isFile.toString());
+	        	fileEntry.name == 'test.json';
+	        	fileEntry.fullPath == 'C:/test.json'
+	        	app.writeFile(fileEntry, null);
+
+	    	});
+
+		});
+	},
+
+	writeFile: function(fileEntry, dataObj) {
+    // Create a FileWriter object for our FileEntry (log.txt).
+    fileEntry.createWriter(function (fileWriter) {
+
+        fileWriter.onwriteend = function() {
+            console.log("Successful file write...");
+            app.readFile(fileEntry);
+        };
+
+        fileWriter.onerror = function (e) {
+            console.log("Failed file write: " + e.toString());
+        };
+
+        // If data object is not passed in,
+        // create a new Blob instead.
+        if (!dataObj) {
+            dataObj = new Blob(['some file data'], { type: 'text/plain' });
+        }
+
+        fileWriter.write(dataObj);
+    });
+	},
+
+	readFile: function(fileEntry) {
+
+    fileEntry.file(function (file) {
+        var reader = new FileReader();
+
+        reader.onloadend = function() {
+            console.log("Successful file read: " + this.result);
+            //	displayFileData(fileEntry.fullPath + ": " + this.result);
+        };
+
+        reader.readAsText(file);
+
+    });
+	},
+
 };
 
 app.initialize();
--- platforms/ios/www/js/index.js
+++ platforms/ios/www/js/index.js
@@ -1,9 +1,13 @@
 
 
 $("#test").click(function(){
+<<<<<<< HEAD
+    app.test();
+=======
     app.ajout(258665);
     app.supprimer(0);
     //https://cordova.apache.org/docs/fr/latest/cordova/storage/localstorage/localstorage.html
+>>>>>>> 8c0d2be20624b8568bb953b04c42e951dc38f37f
 });
 
 
@@ -24,8 +28,14 @@ var app = {
         var date = this.date();
         var heure = this.heure();
         date = date + " - " + heure;
+<<<<<<< HEAD
+        console.log(cordova.file);
+        //alert(date);
+        //setInterval(this.alarm);
+=======
         alert(date);
         //setInterval(this.alarm, 1000);
+>>>>>>> 8c0d2be20624b8568bb953b04c42e951dc38f37f
     },
 
     // Update DOM on a Received Event
@@ -40,12 +50,20 @@ var app = {
         console.log('Received Event: ' + id);
     },
 
+<<<<<<< HEAD
+    date: function(){
+=======
     date: function(date){       // recupere la date sous format jour jour mois annee
+>>>>>>> 8c0d2be20624b8568bb953b04c42e951dc38f37f
         // les noms de jours / mois
         var jours = new Array("dimanche", "lundi", "mardi", "mercredi", "jeudi", "vendredi", "samedi");
         var mois = new Array("janvier", "fevrier", "mars", "avril", "mai", "juin", "juillet", "aout", "septembre", "octobre", "novembre", "decembre");
         // on recupere la date
+<<<<<<< HEAD
+        var date = new Date();
+=======
         if (!date) {var date = new Date();}
+>>>>>>> 8c0d2be20624b8568bb953b04c42e951dc38f37f
         // on construit le message
         var message = jours[date.getDay()] + " ";   // nom du jour
         message += date.getDate() + " ";   // numero du jour
@@ -54,8 +72,13 @@ var app = {
         return message;
     },
 
+<<<<<<< HEAD
+    heure: function(){
+         var date = new Date();
+=======
     heure: function(date){      // recupere la date sous format heure minute
          if (!date) {var date = new Date();}
+>>>>>>> 8c0d2be20624b8568bb953b04c42e951dc38f37f
          var heure = date.getHours();
          var minutes = date.getMinutes();
          if(minutes < 10)
@@ -65,6 +88,67 @@ var app = {
          return heure + "h" + minutes;
     },
 
+<<<<<<< HEAD
+    alarm: function(){
+
+    },
+
+    test: function(){
+    	var jsonArray = {"name": "Chris", "age": "38"};
+		var myJsonString = JSON.stringify(jsonArray);
+		window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, function (fs) {
+
+	    	console.log('file system open: ' + fs.name);
+	    	fs.root.getFile("test.json", { create: true, exclusive: false }, function (fileEntry) {
+
+	        	console.log("fileEntry is file?" + fileEntry.isFile.toString());
+	        	fileEntry.name == 'test.json';
+	        	fileEntry.fullPath == 'C:/test.json'
+	        	app.writeFile(fileEntry, null);
+
+	    	});
+
+		});
+	},
+
+	writeFile: function(fileEntry, dataObj) {
+    // Create a FileWriter object for our FileEntry (log.txt).
+    fileEntry.createWriter(function (fileWriter) {
+
+        fileWriter.onwriteend = function() {
+            console.log("Successful file write...");
+            app.readFile(fileEntry);
+        };
+
+        fileWriter.onerror = function (e) {
+            console.log("Failed file write: " + e.toString());
+        };
+
+        // If data object is not passed in,
+        // create a new Blob instead.
+        if (!dataObj) {
+            dataObj = new Blob(['some file data'], { type: 'text/plain' });
+        }
+
+        fileWriter.write(dataObj);
+    });
+	},
+
+	readFile: function(fileEntry) {
+
+    fileEntry.file(function (file) {
+        var reader = new FileReader();
+
+        reader.onloadend = function() {
+            console.log("Successful file read: " + this.result);
+            //	displayFileData(fileEntry.fullPath + ": " + this.result);
+        };
+
+        reader.readAsText(file);
+
+    });
+	},
+=======
     alarm: function(){      // verifie si les alarmes rentrees sont égales à la date et heure actuelle
         var data = localStorage.getItem('alarme');  
         if (data) {  
@@ -112,6 +196,7 @@ var app = {
 
     alert: function(){   //notification
     },
+>>>>>>> 8c0d2be20624b8568bb953b04c42e951dc38f37f
 
 };
 
